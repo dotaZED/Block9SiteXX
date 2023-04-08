3e92a2d269b43f4f951edb74eb1ecb4540e00504
Kemberly Sandoval <kemberlysandoval@Kemberlys-Air.home>
2023-04-07 20:05:58 -0700

    lets adjust this

--- javascript.js
+++ javascript.js
@@ -1,27 +1,44 @@
 const scrollImages = document.querySelector('.scroll-images');
 const images = document.querySelectorAll('.scroll-images img');
+let width = 0;
 const maxHeight = 300;
-let totalWidth = 0;
 
 images.forEach(image => {
-  const imageWidth = image.clientWidth;
-  const imageHeight = image.clientHeight;
-  const ratio = maxHeight / imageHeight;
-  const newWidth = imageWidth * ratio;
-
+  width += image.clientWidth;
   image.style.height = `${maxHeight}px`;
-  image.style.width = `${newWidth}px`;
-  totalWidth += newWidth;
+  image.style.objectFit = 'cover';
 });
 
-scrollImages.style.width = `${totalWidth}px`;
+scrollImages.style.width = `${width}px`;
 
 let scrollPosition = 0;
-setInterval(() => {
+let intervalId = setInterval(() => {
   scrollPosition++;
   scrollImages.style.transform = `translateX(-${scrollPosition}px)`;
   if (scrollPosition >= images[0].clientWidth) {
     scrollImages.appendChild(images[0]);
     scrollPosition = 0;
   }
-}, 30);
+}, 10);
+
+// Keep scrolling even when window loses focus
+let lastTime = 0;
+const updateInterval = 10;
+
+function updateScroll() {
+  const now = Date.now();
+  const deltaTime = now - lastTime;
+  lastTime = now;
+  
+  scrollPosition += deltaTime / updateInterval;
+  scrollImages.style.transform = `translateX(-${scrollPosition}px)`;
+  
+  if (scrollPosition >= images[0].clientWidth) {
+    scrollImages.appendChild(images[0]);
+    scrollPosition = 0;
+  }
+  
+  requestAnimationFrame(updateScroll);
+}
+
+requestAnimationFrame(updateScroll);
