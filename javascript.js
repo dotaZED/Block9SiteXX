f541347c6718d206df836305d836cdcbd513f737
Kemberly Sandoval <kemberlysandoval@Kemberlys-Air.home>
2023-04-07 19:36:14 -0700

    made an adjustment so that the script is never ending for images

--- javascript.js
+++ javascript.js
@@ -5,3 +5,13 @@ images.forEach(image => {
   width += image.clientWidth;
 });
 scrollImages.style.width = `${width}px`;
+
+let scrollPosition = 0;
+setInterval(() => {
+  scrollPosition++;
+  scrollImages.style.transform = `translateX(-${scrollPosition}px)`;
+  if (scrollPosition >= images[0].clientWidth) {
+    scrollImages.appendChild(images[0]);
+    scrollPosition = 0;
+  }
+}, 10);
